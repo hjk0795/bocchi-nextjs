bc3d35f842e0b996032e023d625370c9def73561
hjk0795 <hjk0795@gmail.com>
2022-10-11 09:23:21 +1300

    deployment error fixed

--- next.config.js
+++ next.config.js
@@ -11,3 +11,9 @@ module.exports = {
     domains: ['images.freeimages.com', 'img.freepik.com', 'cdn-icons-png.flaticon.com'],
   },
 }
+
+module.exports = {
+  // time in seconds of no pages generating during static
+  // generation before timing out
+  staticPageGenerationTimeout: 1000,
+}
