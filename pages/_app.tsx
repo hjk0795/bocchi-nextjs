e724d9d68ac9589ebf6d966d74f70c274cf88178
hjk0795 <hjk0795@gmail.com>
2023-01-14 16:35:09 +1300

    _app.tsx modified

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import Header from "../components/header";
 import Layout from "../components/layout";
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <Header />
